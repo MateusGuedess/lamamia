1856055b36acfd384b1f3ef8eed31e28d03cbdd7
Mateus Guedes <mxteusgued@gmail.com>
2023-06-05 23:29:07 -0300

    feat: portfolio page

--- src/app/portfolio/[category]/page.tsx
+++ src/app/portfolio/[category]/page.tsx
@@ -1,10 +1,26 @@
+import Button from '@/components/Button/Button'
+import Image from 'next/image'
 import React from 'react'
+import styles from "./page.module.css"
+
 
 function Category({params}) {
 
   console.log(params)
   return (
-    <div>Category</div>
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="/"/>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image className={styles.img} fill={true} alt="" src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg" />
+        </div>
+      </div>
+    </div>
   )
 }
 
