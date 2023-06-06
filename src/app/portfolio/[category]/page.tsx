1443f0af227e64f8201b9db3c946162937185843
Mateus Guedes <mxteusgued@gmail.com>
2023-06-06 18:30:36 -0300

    feat: finish portfolio page

--- src/app/portfolio/[category]/page.tsx
+++ src/app/portfolio/[category]/page.tsx
@@ -3,13 +3,38 @@ import Image from 'next/image'
 import React from 'react'
 import styles from "./page.module.css"
 
+interface Category {
+  params: {
+    category: string
+  }
+}
 
-function Category({params}) {
 
-  console.log(params)
+function Category({ params }: Category) {
+
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="/"/>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image className={styles.img} fill={true} alt="" src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg" />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="/"/>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image className={styles.img} fill={true} alt="" src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg" />
+        </div>
+      </div>
       <div className={styles.item}>
         <div className={styles.content}>
           <h1 className={styles.title}>Test</h1>
