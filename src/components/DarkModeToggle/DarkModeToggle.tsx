6566893108514254c378f9968b8f4cbbf0408e1c
Mateus Guedes <mxteusgued@gmail.com>
2023-06-19 03:02:42 -0300

    feat: darkMode

---
+++ src/components/DarkModeToggle/DarkModeToggle.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./darkModeToggle.module.css";
+
+function DarkModeToggle() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.icon}>🌙</div>
+      <div className={styles.icon}>🌞</div>
+    </div>
+  );
+}
+
+export default DarkModeToggle;
